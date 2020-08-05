c07647d79b80c6fe3e234313fe8b7bc17a92efd6
sabin <kcsabin6@gmail.com>
2020-08-05 16:16:04 -0400

    Filterdemo

---
+++ src/com/filter/MyFilter.java
@@ -0,0 +1,30 @@
+package com.filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class MyFilter implements Filter {
+	
+	public void init(FilterConfig arg0)throws ServletException {}
+
+	
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		//PrintWriter out= resp.getWriter();
+		System.out.println("Filter is invoked before");
+		
+		chain.doFilter(req, resp);
+		
+		System.out.println("Filter is invoked after");
+		
+	}
+	public void destroy() {}
+
+}
